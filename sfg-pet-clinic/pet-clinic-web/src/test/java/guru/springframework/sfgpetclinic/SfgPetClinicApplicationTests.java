3757e21b2beda2a0b1c254a744ce6f61272bc941
Tomasz Drwięga <tomdrw88@o2.pl>
2019-01-01 10:59:14 +0100

    Refactoring to multimodule maven project. closes #1

---
+++ sfg-pet-clinic/pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,16 @@
+/*
+ *  Company: RS
+ *  Project: pet-clinic-web
+ *  Created: 29 gru 2018  16:42:46
+ *  Author:  RS
+ */
+package guru.springframework.sfgpetclinic;
+
+/**
+ *<p></p><br>29 gru 2018
+ * @author RS
+ *
+ */
+public class SfgPetClinicApplicationTests {
+
+}
